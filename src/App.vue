953d149168e405a7dfcfa31766196454cef737c7
IbrahimHicyilmaz <blog.ibrahimhicyilmaz5334@gmail.com>
2024-02-14 15:42:05 +0300

    08 commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Form />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Form from './components/Form.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
